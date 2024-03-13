be8f78c69e75c60b699604e2cdb9f577a7541cd4
Harleen <155694034+Harleen2711@users.noreply.github.com>
2024-03-12 18:36:10 -0600

    done

---
+++ Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Assignment_1;
+
+class Program
+{
+    static void Main()
+    {
+        var student1 = StudentMethods.CreateStudent(1, StudentType.FullTime, "Harleen", "Kaur");
+        var student2 = StudentMethods.CreateStudent(2, StudentType.FullTime, "Varinder", "Singh");
+        var student3 = StudentMethods.CreateStudent(3, StudentType.PartTime, "Gurshan", "Singh");
+
+        Console.WriteLine("Assignment One");
+
+        StudentMethods.OutputStudent(student1);
+        StudentMethods.OutputStudent(student2);
+        StudentMethods.OutputStudent(student3);
+    }
+}
